f9297f85e36cafc6939ebd63f07b6265af9ac76d
Austin Clements <austin@google.com>
2016-02-01 17:57:57 -0500

    gg: randomize test data order to test sorting

--- gg/testmain.go
+++ gg/testmain.go
@@ -8,6 +8,7 @@ package main
 
 import (
 	"math"
+	"math/rand"
 	"os"
 
 	"github.com/aclements/go-gg/gg"
@@ -16,9 +17,12 @@ import (
 
 func main() {
 	xs := vec.Linspace(-10, 10, 100)
+	for i := range xs {
+		xs[i] = rand.Float64()*20 - 10
+	}
 	ys := vec.Map(math.Sin, xs)
 
 	plot := gg.NewPlot()
-	plot.Bind("x", xs).Bind("y", ys).Add(gg.LayerPaths())
+	plot.Bind("x", xs).Bind("y", ys).Add(gg.LayerLines())
 	plot.WriteSVG(os.Stdout, 200, 100)
 }
