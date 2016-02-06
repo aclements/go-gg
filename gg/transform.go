7e69e4891e789a37bbcc887011f0a242849b73d6
Austin Clements <austin@google.com>
2016-02-06 14:36:16 -0500

    table: implement multi-column SortBy

--- gg/transform.go
+++ gg/transform.go
@@ -6,11 +6,14 @@ package gg
 
 import "github.com/aclements/go-gg/table"
 
-// SortBy sorts each group by column col. If the column's type
+// SortBy sorts each group by the named columns. If a column's type
 // implements sort.Interface, rows will be sorted according to that
 // order. Otherwise, the values in the column must be naturally
 // ordered (their types must be orderable by the Go specification). If
-// neither is true, SortBy panics with a *generic.TypeError.
-func (p *Plot) SortBy(col string) *Plot {
-	return p.SetData(table.SortBy(p.Data(), col))
+// neither is true, SortBy panics with a *generic.TypeError. If more
+// than one column is given, SortBy sorts by the tuple of the columns;
+// that is, if two values in the first column are equal, they are
+// sorted by the second column, and so on.
+func (p *Plot) SortBy(cols ...string) *Plot {
+	return p.SetData(table.SortBy(p.Data(), cols...))
 }
--- table/sort.go
+++ table/sort.go
@@ -10,22 +10,35 @@ import (
 	"github.com/aclements/go-gg/generic"
 )
 
-// SortBy sorts each group of g by the named column. If the column's
+// SortBy sorts each group of g by the named columns. If a column's
 // type implements sort.Interface, rows will be sorted according to
 // that order. Otherwise, the values in the column must be naturally
 // ordered (their types must be orderable by the Go specification). If
-// neither is true, SortBy panics with a *generic.TypeError.
-func SortBy(g Grouping, col string) Grouping {
+// neither is true, SortBy panics with a *generic.TypeError. If more
+// than one column is given, SortBy sorts by the tuple of the columns;
+// that is, if two values in the first column are equal, they are
+// sorted by the second column, and so on.
+func SortBy(g Grouping, cols ...string) Grouping {
 	// TODO: Consider a generic MapConcatGroups.
 
 	// Sort each group.
 	out := Grouping(new(Table))
+	sorters := make([]sort.Interface, len(cols))
 	for _, gid := range g.Tables() {
 		t := g.Table(gid)
-		seq := t.MustColumn(col)
-		sorter := generic.Sorter(seq)
 
-		if sort.IsSorted(sorter) {
+		// Create sorters for each column.
+		sorters = sorters[:0]
+		for _, col := range cols {
+			seq := t.MustColumn(col)
+			sorter := generic.Sorter(seq)
+			if sort.IsSorted(sorter) {
+				continue
+			}
+			sorters = append(sorters, sorter)
+		}
+
+		if len(sorters) == 0 {
 			// Avoid shuffling everything by the identity
 			// permutation.
 			out = out.AddTable(gid, t)
@@ -38,8 +51,8 @@ func SortBy(g Grouping, col string) Grouping {
 			perm[i] = i
 		}
 
-		// Sort the permutation sequence using b as the key.
-		sort.Stable(&permSort{perm, sorter})
+		// Sort the permutation sequence.
+		sort.Stable(&permSort{perm, sorters})
 
 		// Permute all columns.
 		nt := new(Table)
@@ -56,7 +69,7 @@ func SortBy(g Grouping, col string) Grouping {
 
 type permSort struct {
 	perm []int
-	key  sort.Interface
+	keys []sort.Interface
 }
 
 func (s *permSort) Len() int {
@@ -64,7 +77,16 @@ func (s *permSort) Len() int {
 }
 
 func (s *permSort) Less(i, j int) bool {
-	return s.key.Less(s.perm[i], s.perm[j])
+	// Since there's no way to ask about equality, we have to do
+	// extra work for all of the keys except the last.
+	for _, key := range s.keys[:len(s.keys)-1] {
+		if key.Less(s.perm[i], s.perm[j]) {
+			return true
+		} else if key.Less(s.perm[j], s.perm[i]) {
+			return false
+		}
+	}
+	return s.keys[len(s.keys)-1].Less(s.perm[i], s.perm[j])
 }
 
 func (s *permSort) Swap(i, j int) {
